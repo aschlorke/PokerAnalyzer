6063e866862face02c31ddbdb73573dfcf06b8bc
Aaron Schlorke <aschlorke@yahoo.com>
2022-07-16 20:22:12 -0400

    cleanup unused module

--- client/poker-analyzer/src/app/components/playing-card/playing-card.module.ts
+++ client/poker-analyzer/src/app/components/playing-card/playing-card.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PlayingCardComponent } from './playing-card.component';
 
 @NgModule({
@@ -9,7 +8,6 @@ import { PlayingCardComponent } from './playing-card.component';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
   ],
   exports: [
     PlayingCardComponent
